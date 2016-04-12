07929bfccbf492ecb872bbe887006bffce5dd5d2
chenxuesdu <chenxuesdu@gmail.com>
2016-04-12 08:47:00 -0700

    add server.js
    initial commit with function to create user and get all users.
    GET: http://localhost:8080/api/user
    Create: http://localhost:8080/api/user. with user_name in the body.

---
+++ server/server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+var User = require('./app/models/user');
+
+//routes for our api
+var router = express.Router();
+
+//middleware to use for all requests
+router.use(function(req, res, next){
+    console.log("request is happening");
+    next();
+});
+
+router.get('/', function(req, res){
+     res.json({message:'welcome to our api!'});
+});
+
+//post for /api/user
+router.route('/user').
+	post(function(req, res){
+    	var user = new User();
+    	user.user_name = req.body.user_name;
+		user.first_name = req.body.first_name;
+		user.last_name = req.body.last_name;
+		user.password = req.body.password;
+		user.email = req.body.email;
+
+		user.save(function(err){
+			if (err)
+				res.send(err);
+			res.json({message : 'user created!'});
+		})
+	})
+    .get(function(req, res){
+        User.find(function(err, users) {
+            if (err)
+                res.send(err);
+            res.json(users);
+        });
+    });
+	
+app.use('/api', router);
+app.listen(port);
