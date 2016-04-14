c224b448be5255f59b0ba13c3c0570fdd6e4904d
chenjie <jie.chen8275@gmail.com>
2016-04-14 14:08:34 -0700

    update product web page

--- shopping/routes/product.js
+++ shopping/routes/product.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var blogEngine = require('../public/javascripts/blog');
+//console.log( 'The area of a circle of radius 4 is ' + circle.area(4));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('product', { title: 'product' });
+  res.render('product', { title:'product', entries: blogEngine.getBlogEntries() });
 });
 
 module.exports = router;
