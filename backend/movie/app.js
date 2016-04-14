ee962ab34b76f60133dc7ff0137022a9082cac6f
Michael Dong <michael.dong@sjsu.edu>
2016-04-13 19:27:55 -0700

    fixed routing issue

--- backend/movie/app.js
+++ backend/movie/app.js
@@ -44,9 +44,9 @@ app.use(function(req,res,next){
 //assign routers
 app.use('/', routes);
 app.use('/user', user);
-app.use('movie', movie);
-app.use('cart',cart);
-app.use('order',order);
+app.use('/movie', movie);
+app.use('/cart',cart);
+app.use('/order',order);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
