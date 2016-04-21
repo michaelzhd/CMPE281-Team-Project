90b54cd619372e4dbc7c3eb98a955b7871616d15
chenjie <jie.chen8275@gmail.com>
2016-04-21 14:52:31 -0700

    update javascript

--- shopping/public/javascripts/product-function.js
+++ shopping/public/javascripts/product-function.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    $('#tabs').tabs();
+    //$('#tabs').tabs();
 });
+
+$("#button-cart1").click(function(event){
+    $('#cart_total').text = '1 item';
+});
+
+function myFunction(){
+    $('#cart_total').value = '1 item';
+}
+
