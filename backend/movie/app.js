d7c3203193e658d7c4d27b3ae20ed178171e07c0
Michael Dong <michael.dong@sjsu.edu>
2016-04-27 13:33:53 -0700

    added a route category

--- backend/movie/app.js
+++ backend/movie/app.js
@@ -47,6 +47,7 @@ app.use('/user', user);
 app.use('/movie', movie);
 app.use('/cart',cart);
 app.use('/order',order);
+app.use('/category',category);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
